fcb96ef39c037edca5bd8c701f3f7f98b4d363ed
shubharajkharel <shubha.raj.kharel@gmail.com>
2023-10-18 17:30:14 -0400

    hydra callbacks fixed

--- main.py
+++ main.py
@@ -7,10 +7,18 @@ from rich import print
 
 @hydra.main(version_base=None, config_path="cfg", config_name="experiment")
 def main(cfg: DictConfig):
-    print(cfg)
+    # print(cfg)
+    # return
     data_module = instantiate(cfg.data_module)
     model = instantiate(cfg.model)
+
+    model.example_input_array = data_module.train_dataloader().dataset[0][0].to("mps")
+
+    # from utils.src.misc.model_adapters import PLAdapter
+    # print(PLAdapter(model).summary())
+
     trainer = instantiate(cfg.trainer)
+    trainer.callbacks = instantiate(cfg.callbacks).values()
     trainer.fit(model, data_module)
     trainer.test(model, datamodule=data_module)
 
