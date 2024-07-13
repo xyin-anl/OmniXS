274c8df5add96a9f29ca4d7bddc3f0793aaefa40
shubharajkharel <shubha.raj.kharel@gmail.com>
2024-07-13 19:43:06 -0400

    pretty umaps

--- scripts/paper/plot_desc_umap.py
+++ scripts/paper/plot_desc_umap.py
@@ -1,4 +1,9 @@
 # %%
+import datamapplot
+import glasbey
+from matplotlib import colors as mcolors
+from copy import deepcopy
+from sklearn.preprocessing import StandardScaler
 
 import numpy as np
 import pandas as pd
@@ -18,6 +23,7 @@ import pandas as pd
 # %%
 
 
+# LOAD ALL DATA
 def get_desc_df(compound):
     df_desc = pd.read_csv(cfg.paths.descriptors.format(compound=compound))
     df_desc.columns = ["ids", "sites"] + list(df_desc.columns[2:])
@@ -40,22 +46,6 @@ def get_desc_df(compound):
     return df
 
 
-def get_balanced_df_with_unique_ids():
-    df = pd.concat([get_desc_df(c) for c in cfg.compounds])
-    df_unique_dict = {
-        c: df[df.compound == c].drop_duplicates(subset="ids", keep="first")
-        for c in cfg.compounds
-    }
-    COUNT = min([len(d) for d in df_unique_dict.values()])
-    df_unique = pd.concat([d.sample(COUNT) for d in df_unique_dict.values()])
-    df_unique["compound_idx"] = pd.Categorical(
-        df_unique.compound, categories=cfg.compounds
-    ).codes
-    return df_unique
-
-
-df_unique = get_balanced_df_with_unique_ids()
-
 df = pd.concat([get_desc_df(c) for c in cfg.compounds])
 df["compound_idx"] = pd.Categorical(
     df.compound,
@@ -64,126 +54,178 @@ df["compound_idx"] = pd.Categorical(
 
 # %%
 
-# # ORIGNAL PRAMS
-# umap_proj = umap.UMAP(
-#     n_components=2,
-#     n_neighbors=25,
-#     min_dist=0.45,
-#     random_state=7,
-# ).fit_transform(df_unique.features.tolist())
-# umap_proj = umap.UMAP(
-#     n_components=2,
-#     n_neighbors=25,
-#     min_dist=0.45,
-#     random_state=7,
-# ).fit_transform(df_unique.features.tolist())
+# FEATURES UMAP
+desc_umap_features = {
+    "n_components": 2,
+    "n_neighbors": 50,
+    "min_dist": 0.8,
+    "random_state": 7,
+}
 
+umap_proj_features = umap.UMAP(**desc_umap_features).fit_transform(
+    StandardScaler().fit_transform(df.features.tolist())
+    # plot_df.features.tolist()
+)
 # %%
-
-plot_df = df.copy()
-# plot_df = df_unique.copy()
-
-# # # UMAP OF FEATURES
-# features_umap_kwargs = {
-#     "n_components": 2,
-#     "n_neighbors": 50,
-#     "min_dist": 0.45,
-#     "random_state": 7,
-# }
-# umap_proj = umap.UMAP(**features_umap_kwargs).fit_transform(plot_df.features.tolist())
-# plot_df["umap_f0"] = umap_proj[:, 0]
-# plot_df["umap_f1"] = umap_proj[:, 1]
-# X, Y = "umap_f0", "umap_f1"
-
 # SPECTRA UMAP
 spectra_umap_kwargs = {
     "n_components": 2,
-    "n_neighbors": 50,
-    "min_dist": 0.45,
+    "n_neighbors": 25,
+    "min_dist": 0.8,
     "random_state": 7,
 }
 umap_proj_spectra = umap.UMAP(**spectra_umap_kwargs).fit_transform(
-    plot_df.spectras.tolist()
+    StandardScaler().fit_transform(df.spectras.tolist())
+    # plot_df.spectras.tolist()
 )
+## %%
+
+
+# %%
+
+#
+plot_df = df.copy()
+plot_df["umap_f0"] = umap_proj_features[:, 0]
+plot_df["umap_f1"] = umap_proj_features[:, 1]
 plot_df["umap_s0"] = umap_proj_spectra[:, 0]
 plot_df["umap_s1"] = umap_proj_spectra[:, 1]
-X, Y = "umap_s0", "umap_s1"
 
 
 # %%
 
-DESCRIPTORS = ["OS", "CN", "OCN", "NNRS", "MOOD"]
-color_by = ["compound_idx", "OS", "CN", "OCN", "NNRS", "MOOD"]
-cmap_dict = {
-    "compound_idx": "tab10",
-    "OS": "jet",
-    "CN": "jet",
-    "OCN": "jet",
-    "NNRS": "jet",
-    "MOOD": "jet",
+# DEFAULT PARAMETERS
+datamapplot_kwargs = {
+    # "palette_min_lightness": 0,
+    # "palette_hue_shift": 0,
+    # "palette_hue_radius_dependence": 0.1,
+    "color_label_text": True,
+    "color_label_arrows": True,
+    "label_over_points": True,
+    "dynamic_label_size_scaling_factor": 0.5,
+    "dynamic_label_size": False,
+    "max_font_size": 50,
+    "min_font_size": 49,
+    "font_family": "Urbanist",
+    "verbose": True,
+    "darkmode": False,  # label color (and prolly others) also gets affects
+    # "alpha": 1,
+    "add_glow": False,  # SET TO FALSE FOR TESTING, glow makes diffused colors
+    # "marker_color_array": [compound_colors[compound] for compound in labels],
+    "dpi": 300,
+    "noise_label": "nan",
+    "use_medoids": True,  # better but slower
+    "point_size": 3,
+    "arrowprops": {
+        "arrowstyle": "wedge,tail_width=0.1,shrink_factor=0.5",
+        "connectionstyle": "arc3,rad=0.05",
+        "linewidth": 0,
+    },
+    "label_font_size": 50,
+    "label_base_radius": 10,
 }
-FONTSIZE = 22
-# X, Y = "umap_s0", "umap_s1"
 
 
-def series_to_color(s, cmap="jet"):
-    """Utility function to convert a series to a color array for plotting."""
-    select_idx = ~s.isna() & ~np.isinf(s)
+# PARAMETERS
+data_class = "spectra"
+desc = "compound"  # ["compound_idx", "OS", "CN", "OCN", "NNRS", "MOOD]
+datamapplot_kwargs["darkmode"] = True
 
-    if cmap != "tab10":
-        normalizer = Normalize()
-        non_outliers = plt.get_cmap(cmap)(normalizer(s[select_idx]))
-    else:
-        non_outliers = plt.get_cmap(cmap)(s[select_idx])
-    c = np.zeros((len(s), 4))
-    c[select_idx] = non_outliers
-    c[~select_idx] = (0, 0, 0, 0)
-    return c
+# # # FAST/OK
+# # datamapplot_kwargs["add_glow"] = False
+# # datamapplot_kwargs["use_medoids"] = False
+
+# SLOW/BETTER
+datamapplot_kwargs["add_glow"] = True
+datamapplot_kwargs["use_medoids"] = True
+
+datamapplot_kwargs["alpha"] = 1
+if desc == "OCN":
+    datamapplot_kwargs["label_over_points"] = False
+    if data_class == "feature":
+        datamapplot_kwargs["label_direction_bias"] = 2
+        datamapplot_kwargs["label_base_radius"] = 20
+        datamapplot_kwargs["label_over_points"] = False
+    elif data_class == "spectra":
+        datamapplot_kwargs["label_direction_bias"] = 4  # makes label more vertical
+        datamapplot_kwargs["label_base_radius"] = 19
 
 
-fig = plt.figure(figsize=(15, 20))
-plt.style.use(["default", "science"])
-gs = fig.add_gridspec((len(color_by) + 1) // 2, 2, hspace=0, wspace=0)
-axs = gs.subplots(sharex=True, sharey=True)
-for ax, d in zip(axs.flatten(), color_by):
+# less thatn 5% of maximum count
+COUNT_THRESHOLD = 0.05 * plot_df[desc].value_counts().max()
+plot_df[desc] = plot_df[desc].replace(
+    plot_df[desc].value_counts()[plot_df[desc].value_counts() < COUNT_THRESHOLD].index,
+    np.nan,
+)
+labels = plot_df[desc]
 
-    # # COLOR EACH """ COMPOUND BLOCK AT A TIME
-    # for c in cfg.compounds:
-    #     ax.scatter(
-    #         plot_df[plot_df.compound == c][X],
-    #         plot_df[plot_df.compound == c][Y],
-    #         c=series_to_color(plot_df[plot_df.compound == c][d], cmap=cmap_dict[d]),
-    #         s=0.8,
-    #         # alpha=0.8,
-    #         label=c,
-    #     )
-    # fig.suptitle("Local color scaling", fontsize=FONTSIZE * 1.2, y=0.9)
-    # filename = f"umap_desc_{X}_{Y}_local.pdf"
+if desc in ["OS", "CN"]:
+    labels = labels.fillna(9999).astype("int64").astype("str").replace("9999", "nan")
 
-    # # COLOR ALL POINTS AT ONCE
-    ax.scatter(
-        plot_df[X],
-        plot_df[Y],
-        c=series_to_color(plot_df[d], cmap=cmap_dict[d]),
-        s=2,
-        alpha=0.8,
+if desc == "OCN":
+    labels = (
+        labels.fillna(9999)
+        .apply(lambda x: np.round(x * 2) / 2)  # Round to nearest 0.5
+        .apply(
+            lambda x: int(x) if x.is_integer() else x
+        )  # Convert to int if whole number
+        .astype(str)
+        .replace("9999.0", "nan")
     )
-    fig.suptitle("Global color scaling", fontsize=FONTSIZE * 1.2, y=0.9)
-    filename = f"umap_desc_{X}_{Y}_global.pdf"
 
-    # title top top left inside of the axes
-    if d != "compound_idx":
-        ax.set_title(d, fontsize=FONTSIZE, loc="left", x=0.01, y=0.9)
-    else:
-        ax.set_title("Element", fontsize=FONTSIZE, loc="left", x=0.01, y=0.9)
-    ax.set_xticks([])
-    ax.set_yticks([])
 
-fig.savefig(filename, dpi=500, bbox_inches="tight")
+if desc in ["OS", "CN", "OCN"]:
+    glasbey_palette = glasbey.create_palette(
+        palette_size=len(labels.unique()),
+        # lightness_bounds=(50, 100),
+        # chroma_bounds=(40, 80),
+        # colorblind_safe=True,
+    )
+    # datamapplot_kwargs["title"] = desc
+    color_dict = {}
+    for i, label in enumerate(labels.unique()):
+        if label == "nan":
+            color_dict[label] = "#999999"  # default grey in datamapplot
+        else:
+            color_dict[label] = glasbey_palette[i - 1]
+
+if desc == "compound":
+    color_dict = {
+        c: mcolors.to_hex(plt.get_cmap("tab10")(i)) for i, c in enumerate(cfg.compounds)
+    }
+    # datamapplot_kwargs["title"] = "Element"
+
+
+X = f"umap_{data_class[0].lower()}0"
+Y = f"umap_{data_class[0].lower()}1"
+points = np.array([plot_df[X].values, plot_df[Y].values]).T
+fig, ax = datamapplot.create_plot(
+    points,
+    labels,
+    marker_color_array=(
+        [color_dict[compound] for compound in labels] if color_dict else None
+    ),
+    label_color_map=color_dict,
+    highlight_labels=set(labels),
+    **datamapplot_kwargs,
+)
+
+ax.set_facecolor("#1F1F1F")
+fig.patch.set_facecolor("#696969")
+
+
+y_label = desc if desc != "compound" else "Element"
+ax.set_ylabel(y_label, fontsize=32, fontweight="bold", color="white")
+
+ax.set_title(
+    "Features" if data_class == "feature" else "Spectras",
+    fontsize=32,
+    fontweight="bold",
+    color="white",
+)
+
+fig.tight_layout()
+fig.savefig(f"umap_{desc}_{data_class}.png", bbox_inches="tight", dpi=300)
+fig.savefig(f"umap_{desc}_{data_class}.pdf", bbox_inches="tight", dpi=300)
+
 
 # %%
-plot_df.to_csv("umap_descriptors.csv", index=False)
-
-# %%
-
-plot_df.NNRS.hasnans
