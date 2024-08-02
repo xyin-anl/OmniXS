0eba03f9685d8a58723aed73cd8062ed46a9eca2
shubharajkharel <shubha.raj.kharel@gmail.com>
2024-08-02 10:30:24 -0400

    r2 score

--- src/models/trained_models.py
+++ src/models/trained_models.py
@@ -1,25 +1,21 @@
 import os
-from typing import Union
-from typing import Literal
-from sklearn.svm import SVR
-from sklearn.linear_model import ElasticNet, Ridge
 from abc import ABC, abstractmethod
 from functools import cached_property
-from typing import List
-from xgboost import XGBRegressor
-
-from sklearn.ensemble import RandomForestRegressor
+from typing import List, Literal, Union
 
 import numpy as np
 import optuna
 import torch
 from hydra.utils import instantiate
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import ElasticNet, LinearRegression, Ridge
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.svm import SVR
+from xgboost import XGBRegressor
 
 from config.defaults import cfg
-from src.models.xas_fc import FC_XAS
 from src.data.ml_data import DataQuery, load_xas_ml_data
+from src.models.xas_fc import FC_XAS
 
 
 class PreTrainedFCXASModel(FC_XAS):
@@ -138,6 +134,10 @@ class TrainedModel(ABC):  #
             [error[idx] for error, idx in zip(self.model.absolute_errors, max_idx)]
         )
         return peak_errors
+
+    @property
+    def r2(self):
+        return r2_score(self.data.test.y, self.predictions)
 
 
 class MeanModel(TrainedModel):
