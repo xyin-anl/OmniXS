9f5f41de33b9d0fe04d022bafe66218f2a92f5b7
shubharajkharel <shubha.raj.kharel@gmail.com>
2024-09-07 19:35:14 -0400

    replot: datasize

--- scripts/paper/data_size_histogram.py
+++ scripts/paper/data_size_histogram.py
@@ -1,4 +1,6 @@
 # %%
+import matplotlib as mpl
+import warnings
 import os
 import pickle
 from utils.src.plots.heatmap_of_lines import heatmap_of_lines
@@ -10,6 +12,7 @@ from src.data.ml_data import load_all_data, load_xas_ml_data, DataQuery
 from src.data.vasp_data_raw import RAWDataVASP
 from src.data.feff_data_raw import RAWDataFEFF
 from src.data.vasp_data import VASPData
+import matplotlib.ticker as ticker
 
 # %%
 
@@ -21,12 +24,16 @@ def get_data_sizes(compound, simulation_type):
 
     pre_filter_size = data_count(
         load_xas_ml_data(
-            DataQuery(compound, simulation_type), filter_spectra_anomalies=False
+            DataQuery(compound, simulation_type),
+            filter_spectra_anomalies=False,
+            use_cache=False,
         )
     )
     post_filter_size = data_count(
         load_xas_ml_data(
-            DataQuery(compound, simulation_type), filter_spectra_anomalies=True
+            DataQuery(compound, simulation_type),
+            filter_spectra_anomalies=True,
+            use_cache=False,
         )
     )
     anamolies = pre_filter_size - post_filter_size
@@ -64,6 +71,19 @@ def get_data_sizes(compound, simulation_type):
     }
 
 
+if os.path.exists("data_size.pkl"):
+    warnings.warn("Using cached data")
+    data = pickle.load(open("data_size.pkl", "rb"))
+else:
+    data = []
+    for compound in cfg.compounds:
+        data.append(get_data_sizes(compound, "FEFF"))
+    data.append(get_data_sizes("Cu", "VASP"))
+    data.append(get_data_sizes("Ti", "VASP"))
+    pickle.dump(data, open("data_size.pkl", "wb"))
+data = np.array(data)
+
+
 # %%
 
 
@@ -73,89 +93,33 @@ def hex_to_rgb(hex_color):
     return tuple(int(hex_color[i : i + 2], 16) / 255.0 for i in (0, 2, 4))
 
 
-def darken_color(color, factor=0.8):
+def darken_color(color, factor=0.4):
     if isinstance(color, str):
         color = hex_to_rgb(color)
     return tuple(max(0, min(1, c * factor)) for c in color)
 
 
-nice_colors = [
-    "#4e79a7",
-    "#76b7b2",
-    "#ff9da7",
-    "#9b59b6",
-    "#3498db",
-    "#95a5a6",
-    "#e74c3c",
-    "#b07aa1",
-]
-
-compound_colors = {c: nice_colors[i] for i, c in enumerate(cfg.compounds)}
-
-
-if os.path.exists("data_size.pkl"):
-    data = pickle.load(open("data_size.pkl", "rb"))
-else:
-    data = []
-    for compound in cfg.compounds:
-        data.append(get_data_sizes(compound, "FEFF"))
-    data.append(get_data_sizes("Cu", "VASP"))
-    data.append(get_data_sizes("Ti", "VASP"))
-data = np.array(data)
-
-
+FONTSIZE = 20
 hatches = {
     "Anomalies": "xxxxx",
     "Unconverged": "/////",
     "ML": None,
 }
 
-import matplotlib as mpl
 
-ASPECT_RATIO = 4 / 3
-HEIGHT = 6
-WEIGHT = HEIGHT * ASPECT_RATIO
-DPI = 300
-FONTSIZE = 14
-plt.style.use(["default", "science", "tableau-colorblind10"])
-mpl.rcParams["font.size"] = FONTSIZE
-mpl.rcParams["axes.labelsize"] = FONTSIZE
-mpl.rcParams["xtick.labelsize"] = FONTSIZE
-mpl.rcParams["ytick.labelsize"] = FONTSIZE
-mpl.rcParams["legend.fontsize"] = FONTSIZE
-mpl.rcParams["figure.dpi"] = DPI
-mpl.rcParams["figure.figsize"] = (WEIGHT, HEIGHT)
-mpl.rcParams["savefig.dpi"] = DPI
-mpl.rcParams["savefig.format"] = "pdf"
-mpl.rcParams["savefig.bbox"] = "tight"
-
-# colors_ml = "#1b9e77"
-# colors_anomalies = "#d95f02"
-# colors_unconverged = "#7570b3"
-
-tableau_colorblind10 = [
-    "#006BA4",
-    "#FF800E",
-    "#ABABAB",
-    "#595959",
-    "#5F9ED1",
-    "#C85200",
-    "#898989",
-    "#A2C8EC",
-    "#FFBC79",
-    "#CFCFCF",
-]
-colors_ml = tableau_colorblind10[0]
-colors_anomalies = tableau_colorblind10[1]
-colors_unconverged = tableau_colorblind10[2]
+colors_ml = plt.get_cmap("tab10")(0)
+colors_anomalies = plt.get_cmap("tab10")(1)
+colors_unconverged = plt.get_cmap("tab10")(2)
+compound_colors = {c: plt.get_cmap("tab10")(i) for i, c in enumerate(cfg.compounds)}
 
 
-cmap = "tab10"
-compound_colors = plt.get_cmap(cmap)(np.linspace(0, 1, len(cfg.compounds) + 2))
+plt.style.use(["default", "science"])
+compound_colors = plt.get_cmap("tab10")(np.linspace(0, 1, len(cfg.compounds) + 2))
 compound_colors = {
     c: compound_colors[i]
     for i, c in enumerate(cfg.compounds + ["Ti\nVASP", "Cu\nVASP"])
 }
+
 hatches = {
     "ML": None,
     "Anomalies": "||",
@@ -168,7 +132,10 @@ hatches = {
 #     "ft_tl": "",
 # }
 
-fig, ax = plt.subplots(1, 1, figsize=(WEIGHT, HEIGHT), dpi=DPI)
+ASPECT_RATIO = 4 / 3
+HEIGHT = 6
+WEIGHT = HEIGHT * ASPECT_RATIO
+fig, ax = plt.subplots(1, 1, figsize=(WEIGHT, HEIGHT), dpi=300)
 for i, compound in enumerate(cfg.compounds + ["Ti\nVASP", "Cu\nVASP"]):
     ax.bar(
         i,
@@ -178,9 +145,9 @@ for i, compound in enumerate(cfg.compounds + ["Ti\nVASP", "Cu\nVASP"]):
         hatch=hatches["ML"],
         label=compound,
         # edgecolor="black",
-        # edgecolor=darken_color(compound_colors[compound.split("\n")[0]]),
+        edgecolor=darken_color(compound_colors[compound.split("\n")[0]]),
         zorder=3,
-        edgecolor="black",
+        # edgecolor="black",
     )
     ax.bar(
         i,
@@ -188,14 +155,15 @@ for i, compound in enumerate(cfg.compounds + ["Ti\nVASP", "Cu\nVASP"]):
         bottom=data[i]["ml"],
         # color=colors_anomalies,
         color=compound_colors[compound],
-        # hatch=hatches["Anomalies"],
-        alpha=0.6,
+        hatch=hatches["Anomalies"],
+        alpha=0.8,
         # edgecolor="black",
         # fill=None,
         # hatch=hatches["Anomalies"],
-        # edgecolor=darken_color(compound_colors[compound.split("\n")[0]]),
+        edgecolor=darken_color(compound_colors[compound.split("\n")[0]]),
+        # edgecolor="black",
         zorder=3,
-        edgecolor="black",
+        # edgecolor="black",
     )
     ax.bar(
         i,
@@ -203,17 +171,25 @@ for i, compound in enumerate(cfg.compounds + ["Ti\nVASP", "Cu\nVASP"]):
         bottom=data[i]["ml"] + data[i]["anomalies"],
         color=compound_colors[compound],
         # color=colors_unconverged,
-        alpha=0.3,
+        alpha=0.55,
         # edgecolor="black",
         # fill=None,
-        # hatch=hatches["Unconverged"],
-        # edgecolor=darken_color(compound_colors[compound.split("\n")[0]]),
-        edgecolor="black",
+        hatch=hatches["Unconverged"],
+        edgecolor=darken_color(compound_colors[compound.split("\n")[0]]),
+        # edgecolor="black",
         zorder=3,
         # hatch=hatches["Unconverged"],
     )
     ax.set_xticks(range(len(cfg.compounds) + 2))
-    ax.set_xticklabels(cfg.compounds + ["Ti\nVASP", "Cu\nVASP"])
+    ax.set_xticklabels(
+        cfg.compounds + ["Ti\nVASP", "Cu\nVASP"], fontsize=FONTSIZE * 0.8
+    )
+
+    ax.yaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=True))
+    ax.ticklabel_format(style="sci", axis="y", scilimits=(3, 3))
+    ax.tick_params(axis="y", which="major", labelsize=FONTSIZE * 0.8)
+    ax.yaxis.get_offset_text().set_size(FONTSIZE * 0.8)
+
 
 # add hatches for legend
 from matplotlib.patches import Patch
@@ -243,7 +219,7 @@ ax.legend(
 ax.grid(axis="y", alpha=0.3, zorder=0)
 ax.set_xlabel("Compound", fontsize=FONTSIZE * 1.2, labelpad=-10)
 ax.set_ylabel("Number of Spectra", fontsize=FONTSIZE * 1.2)
-ax.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, loc: "{:,}".format(int(x))))
+# ax.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, loc: "{:,}".format(int(x))))
 plt.tight_layout()
 plt.savefig("data_size.pdf")
 
