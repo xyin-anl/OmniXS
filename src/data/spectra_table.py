5d7295eac25a5e66c44fd85ae8d4dbed1ae52633
shubharajkharel <shubha.raj.kharel@gmail.com>
2024-10-02 10:23:50 -0400

    runtime estimates

--- src/data/spectra_table.py
+++ src/data/spectra_table.py
@@ -5,7 +5,7 @@ import pickle
 from functools import cached_property
 from typing import List
 
-import appdirs
+# import appdirs
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import pyplot as plt
