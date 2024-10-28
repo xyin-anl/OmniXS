1a56dafdf5e036042a73f915ea2e5744a66fc80f
shubharajkharel <shubha.raj.kharel@gmail.com>
2024-10-28 11:05:19 -0700

    feff data load fix

--- refactor/data/merge_ml_splits.py
+++ refactor/data/merge_ml_splits.py
@@ -73,7 +73,7 @@ class FEFFSplits(MergedSplits):
     def __new__(cls, *args, **kwargs):
         merged = MergedSplits.load(
             FEFFDataTags,
-            DEFAULTFILEHANDLER,
+            DEFAULTFILEHANDLER(),
             balanced=cls.balanced,
             **kwargs,
         )
